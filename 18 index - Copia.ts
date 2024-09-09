d63551af65002f380208d9328a0f5aa3f7d22999
CleitonAguiar22 <cleitonmik2022@gmail.com>
2024-09-09 16:54:08 -0300

    Delete 18 index - Copia.ts

--- 18 index - Copia.ts
+++
@@ -1,8 +0,0 @@
-function inversão(string: string) {
-  let stringArray = string.split("");
-  let revertArray = stringArray.reverse();
-  let arrayString = revertArray.join("");
-  return arrayString;
-}
-
-console.log(inversão("Enaile"));
